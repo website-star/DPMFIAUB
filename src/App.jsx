7f31cebad6e474f4462f839714e44a6d4a25b05d
mucycle-kelompok4 <wadawwadaw204@gmail.com>
2024-11-02 06:07:51 +0700

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './assets/component/navbar'
 import Footer from './assets/component/footer'
 import Home from './assets/pages/Home'
@@ -18,13 +18,11 @@ function App() {
     <>
       <div className='h-screen'>
         <Navbar/>
-          <Router>
             <Routes>
               <Route path='/' element={<Home />}></Route>
               <Route path='/Programs' element={<Programs />}></Route>
               <Route path='/People' element={<Peoples />}></Route>
             </Routes>
-          </Router>
         <footer className='h-[350px] bg-gradient-to-l from-[#3bbdac] from-0% via-[#2BA097] via-30% via-[#256a6b] via-65% to-[#148b8f] to-95% rounded-t-[70px] mt-auto'>
           <Footer/>
         </footer>
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
