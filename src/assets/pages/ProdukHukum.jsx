46ae06c5ed71dc3482516b470a345e1cb2b9a2ff
mucycle-kelompok4 <wadawwadaw204@gmail.com>
2024-11-09 01:49:56 +0700

    produk hukum page responsive

--- src/assets/pages/ProdukHukum.jsx
+++ src/assets/pages/ProdukHukum.jsx
@@ -14,81 +14,81 @@ export default function ProdukHukum() {
             <span className='font-SFLight text-3xl text-justify w-3/4'>“<span className='font-SFMedium'>The clear guidance</span>, <span className='font-SFMedium'>rights and responsibilities estabilishment</span>, and <span className='font-SFMedium'>standards</span> within the campus community."</span>
         </div>
         <div className='min-h-screen flex justify-center mb-28'>
-            <div className='w-11/12 place-items-center grid lg:grid-cols-3 grid-cols-2 px-4 lg:h-[100rem] h-[120rem]'>
-                <a href='https://drive.google.com/file/d/1YROE2eqXG_-5Q4YXXXCs6Z6JfeBYz18F/view' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+            <div className='lg:w-11/12 w-full place-items-center grid lg:grid-cols-3 grid-cols-2 px-4 lg:h-[100rem] h-[95rem] gap-x-5'>
+                <a href='https://drive.google.com/file/d/1YROE2eqXG_-5Q4YXXXCs6Z6JfeBYz18F/view' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={uuadvo} alt="" className='h-72 w-30'/>
+                            <img src={uuadvo} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>UU Advokasi Kelembagaan</span>
-                            <span className='font-SFRegular'>Tahun 2024</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>UU Advokasi Kelembagaan</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2024</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/1LB2q5mVVIB8uQbxgRaCzh_UXBgz6yHpv/view?usp=sharing' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/1LB2q5mVVIB8uQbxgRaCzh_UXBgz6yHpv/view?usp=sharing' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={SOP} alt="" className='h-72 w-30'/>
+                            <img src={SOP} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>SOP Pengawasan BEM FIA</span>
-                            <span className='font-SFRegular'>Tahun 2024</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>SOP Pengawasan BEM FIA</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2024</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/1Rpmsod58D7uNYUePWFUxhGEDmb-tjg0U/view' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/1Rpmsod58D7uNYUePWFUxhGEDmb-tjg0U/view' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={uupkkmb} alt="" className='h-72'/>
+                            <img src={uupkkmb} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>UU PKKMB FIA UB</span>
-                            <span className='font-SFRegular'>Tahun 2024</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>UU PKKMB FIA UB</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2024</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/1Q51xNUXk9Dot4-qOjV9PpKcWmFPHwxkQ/view?usp=sharing' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/1Q51xNUXk9Dot4-qOjV9PpKcWmFPHwxkQ/view?usp=sharing' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={uupemilwa} alt="" className='h-72'/>
+                            <img src={uupemilwa} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>UU Pemilwa FIA UB</span>
-                            <span className='font-SFRegular'>Tahun 2023</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>UU Pemilwa FIA UB</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2023</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/1vW9swsgLmEtrjrO-tA1mEEN1pVQSlnnV/view?usp=sharing' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/1vW9swsgLmEtrjrO-tA1mEEN1pVQSlnnV/view?usp=sharing' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={uup3u} alt="" className='h-72'/>
+                            <img src={uup3u} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>UU P3U</span>
-                            <span className='font-SFRegular'>Tahun 2023</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>UU P3U</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2023</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/1uP_Xupry8NvZBLQlCPzP8z5IVUiiCkoJ/view?usp=sharing' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/1uP_Xupry8NvZBLQlCPzP8z5IVUiiCkoJ/view?usp=sharing' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={uupengawasan} alt="" className='h-72'/>
+                            <img src={uupengawasan} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>UU Pengawasan BEM FIA</span>
-                            <span className='font-SFRegular'>Tahun 2022</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>UU Pengawasan BEM FIA</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2022</span>
                         </div>
                     </div>
                 </a>
-                <a href='https://drive.google.com/file/d/18opNeiXOwhugbVIb3bC-Z4qO_TYSAsLs/view?usp=sharing' target='__blank' rel='noopener noreferrer'>
-                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D] h-[26rem] lg:w-[22rem] w-[20rem] flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200'>
+                <a href='https://drive.google.com/file/d/18opNeiXOwhugbVIb3bC-Z4qO_TYSAsLs/view?usp=sharing' target='__blank' rel='noopener noreferrer' className='lg:h-[26rem] h-[22rem] lg:w-[22rem] w-[11rem]'>
+                    <div className='bg-gradient-to-tr from-[#D64E82] via-15% via-[#BB4071] via-40% via-[#A13360] via-65% via-[#86254E] to-[#6B183D]  flex flex-col rounded-md transform hover:scale-105 transition-transform duration-200 h-full'>
                         <div className='h-[80%] p-2 flex justify-center items-center'>
-                            <img src={adart} alt="" className='h-72'/>
+                            <img src={adart} alt="" className='lg:h-72 h-60 lg:w-30'/>
                         </div>
                         <div className='bg-white rounded-b-md shadow-md p-4 flex flex-col items-center h-[20%]'>
-                            <span className='font-SFSemiBold'>AD/ART LKM FIA</span>
-                            <span className='font-SFRegular'>Tahun 2015</span>
+                            <span className='font-SFSemiBold lg:text-lg text-[12px]'>AD/ART LKM FIA</span>
+                            <span className='font-SFRegular lg:text-md text-[10px]'>Tahun 2015</span>
                         </div>
                     </div>
                 </a>
